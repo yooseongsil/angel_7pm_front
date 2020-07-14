a7fc07cc9692d6512a0eb6b03bf802cd5f68b4de
hanaKim <hananumber1@nate.com>
2020-07-14 23:02:28 +0900

    [add] signin

--- src/views/SignInPage.vue
+++ src/views/SignInPage.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>SignInPage</div>
+  <div>
+    kasdhfkjahsdbfh
+  </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
-  name: 'SignInPage'
+  name: 'SignInPage',
+  data: () => ({
+    email: null,
+    password: null
+  }),
+  created () {
+    this.singIn()
+  },
+  methods: {
+    singIn () {
+      axios({
+        method: 'POST',
+        url: '/accounts/sing-in',
+        data: {
+          email: 'paikend@gmail.com',
+          password: 'admin12345'
+        }
+      }).then(function (response) {
+        console.log(response)
+      })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
