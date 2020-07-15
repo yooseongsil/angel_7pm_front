998581ba6599b23a8203856c509783d31cebdff8
hanaKim <hananumber1@nate.com>
2020-07-15 21:23:56 +0900

    [add] input

--- src/views/SignInPage.vue
+++ src/views/SignInPage.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    kasdhfkjahsdbfh
+    <v-col cols="12" sm="6" md="3">
+      <v-text-field
+        label="Filled"
+        filled
+      ></v-text-field>
+    </v-col>
+    <v-col cols="12" sm="6">
+      <v-text-field
+        v-model="password"
+        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+        :rules="[rules.required, rules.min]"
+        :type="show1 ? 'text' : 'password'"
+        name="input-10-1"
+        label="Normal with hint text"
+        hint="At least 8 characters"
+        counter
+        @click:append="show1 = !show1"
+      ></v-text-field>
+    </v-col>
+
   </div>
 </template>
 
@@ -19,7 +38,7 @@ export default {
     singIn () {
       axios({
         method: 'POST',
-        url: `${this.$store.state.host}/accounts/sing-in/`,
+        url: `${this.$store.state.host}/accounts/sign-in/`,
         data: {
           email: 'test@test.com',
           password: 'admin12345'
