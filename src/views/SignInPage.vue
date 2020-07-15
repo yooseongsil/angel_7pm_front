ba1f436e36b994770f7be95062fa74e3f429e17d
hanaKim <hananumber1@nate.com>
2020-07-15 22:46:06 +0900

    [add] 회원가입 초안

--- src/views/SignInPage.vue
+++ src/views/SignInPage.vue
@@ -1,44 +1,41 @@
 <template>
-  <div>
+  <div id="signIn">
     <!--container-->
     <v-container>
-      <!--개인/기업 라디오 버-->
-    <v-radio-group row>
-      <v-radio label="개인" value="radio-1"></v-radio>
-      <v-radio label="기업" value="radio-2"></v-radio>
-    </v-radio-group>
       <!--아이디 입력-->
-    <v-col cols="12" >
-      <v-text-field
-        v-model="email"
-        label="이메일"
-        filled
-      ></v-text-field>
-    </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="email"
+          label="이메일"
+          filled
+        ></v-text-field>
+      </v-col>
       <!--비밀번호 입력-->
-    <v-col cols="12">
-      <v-text-field
-        v-model="password"
-        label="비밀번호"
-        counter
-      ></v-text-field>
-      <!--@click:append="show1 = !show1"
-        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-        :rules="[rules.required, rules.min]"
-        :type="show1 ? 'text' : 'password'"
-        name="input-10-1"-->
-    </v-col>
-
-    <template>
-      <v-btn block class="deep-purple accent-2"
-      @click="singIn">회원가입</v-btn>
-    </template>
+      <v-col cols="12">
+        <v-text-field
+          v-model="password"
+          label="비밀번호"
+          counter
+        ></v-text-field>
+        <!--@click:append="show1 = !show1"
+          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required, rules.min]"
+          :type="show1 ? 'text' : 'password'"
+          name="input-10-1"-->
+      </v-col>
+      <v-col cols="12">
+        <v-btn block class="deep-purple accent-2"
+               @click="singIn">로그인
+        </v-btn>
+        <v-btn outlined block class="mt-3">회원가입</v-btn>
+      </v-col>
     </v-container>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+
 export default {
   name: 'SignInPage',
   data: () => ({
--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -1,10 +1,71 @@
 <template>
-  <div>SignUpPage</div>
+  <div id="signUp">
+    <!--container-->
+    <v-container>
+      <!--아이디 입력-->
+      <v-col cols="12">
+        <v-text-field
+          v-model="email"
+          label="이메일"
+          filled
+        ></v-text-field>
+      </v-col>
+      <!--비밀번호 입력-->
+      <v-col cols="12">
+        <v-text-field
+          v-model="password"
+          label="비밀번호"
+          counter
+        ></v-text-field>
+      </v-col>
+      <!--비밀번호 입력-->
+      <v-col cols="12">
+        <v-text-field
+          v-model="checkPassword"
+          label="비밀번호 확인"
+          counter
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-row>
+          <v-col cols="6">
+            <v-text-field
+              v-model="belong"
+              label="소속"
+              filled
+            ></v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+              v-model="dep"
+              label="직무"
+              filled
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+      </v-col>
+      <v-col cols="12">
+        <v-btn block class="deep-purple accent-2"
+               >회원가입
+        </v-btn>
+      </v-col>
+    </v-container>
+  </div>
 </template>
 
 <script>
-
+// import axios from 'axios'
 export default {
-  name: 'SignUpPage'
+  name: 'SignUpPage',
+  data: () => ({
+    email: null,
+    password: null,
+    checkPassword: null,
+    belong: null,
+    dep: null
+  }),
+  methods: {
+  }
 }
 </script>
