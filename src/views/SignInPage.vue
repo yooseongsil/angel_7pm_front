3038d85572a2dad4bf1bc07ded3aeceb691d5b50
hanaKim <hananumber1@nate.com>
2020-07-15 22:04:45 +0900

    [add] 로그인 초안

--- src/views/SignInPage.vue
+++ src/views/SignInPage.vue
@@ -1,25 +1,38 @@
 <template>
   <div>
-    <v-col cols="12" sm="6" md="3">
+    <!--container-->
+    <v-container>
+      <!--개인/기업 라디오 버-->
+    <v-radio-group row>
+      <v-radio label="개인" value="radio-1"></v-radio>
+      <v-radio label="기업" value="radio-2"></v-radio>
+    </v-radio-group>
+      <!--아이디 입력-->
+    <v-col cols="12" >
       <v-text-field
-        label="Filled"
+        v-model="email"
+        label="이메일"
         filled
       ></v-text-field>
     </v-col>
-    <v-col cols="12" sm="6">
+      <!--비밀번호 입력-->
+    <v-col cols="12">
       <v-text-field
         v-model="password"
+        label="비밀번호"
+        counter
+      ></v-text-field>
+      <!--@click:append="show1 = !show1"
         :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
         :rules="[rules.required, rules.min]"
         :type="show1 ? 'text' : 'password'"
-        name="input-10-1"
-        label="Normal with hint text"
-        hint="At least 8 characters"
-        counter
-        @click:append="show1 = !show1"
-      ></v-text-field>
+        name="input-10-1"-->
     </v-col>
 
+    <template>
+      <v-btn block class="deep-purple accent-2">회원가입</v-btn>
+    </template>
+    </v-container>
   </div>
 </template>
 
@@ -40,11 +53,13 @@ export default {
         method: 'POST',
         url: `${this.$store.state.host}/accounts/sign-in/`,
         data: {
-          email: 'test@test.com',
-          password: 'admin12345'
+          email: this.email,
+          password: this.password
         }
       }).then(({ data }) => {
-        console.log(data)
+        // console.log(data)
+        document.cookie = `accessToken=${data.token}`
+        axios.defaults.headers.common['x-access-token'] = data.token
       })
         .catch(({ error }) => {
           console.log(error)
