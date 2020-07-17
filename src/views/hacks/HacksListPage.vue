1fbc066332906bdaad2aec5a94053febf269f7ba
Donghoon Song <thdehdgns@gmail.com>
2020-07-17 22:47:37 +0900

    [add] hack list 연동 완료

--- src/views/hacks/HacksListPage.vue
+++ src/views/hacks/HacksListPage.vue
@@ -10,9 +10,9 @@
         <v-card>
           <v-list-item three-line>
             <v-list-item-content>
-              <div class="overline mb-4 teal--text accent-3">모인 금액: {{}}원</div>
+              <div class="overline mb-4 teal--text accent-3">모인 금액: {{hack.total_fee}}원</div>
               <v-list-item-title class="headline mb-1">{{hack.title}}</v-list-item-title>
-              <v-list-item-subtitle>현재 {{}}명 신청</v-list-item-subtitle>
+              <v-list-item-subtitle>현재 {{hack.current_personnel}}명 신청</v-list-item-subtitle>
               <v-list-item-content>(팀별 {{hack.team_personnel}}명)</v-list-item-content>
             </v-list-item-content>
 
@@ -55,14 +55,14 @@ export default {
         url: `${this.$store.state.host}/hacks/`
       }).then(({ data }) => {
         console.log(data)
-        this.hackLists.push(data)
+        this.hackLists = data.results
       })
         .catch(({ error }) => {
           console.log(error)
         })
     },
     gotoDetail (id) {
-      window.location.href = '/hacks/' + id
+      this.$router.push(`/hacks/${id}`)
     }
   },
   created () {
