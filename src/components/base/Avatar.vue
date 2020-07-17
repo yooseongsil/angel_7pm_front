4ccafe0587a24a6001a5ef3f1e742e4bfde08fb1
seongsil <yooo1201@naver.com>
2020-07-18 00:35:28 +0900

    [add] modal, avatar component

---
+++ src/components/base/Avatar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-avatar color="#bb86fc">
+    <span class="black--text headline">{{avatarText}}</span>
+  </v-avatar>
+</template>
+
+<script>
+export default {
+  name: 'Avatar',
+  props: ['email'],
+  computed: {
+    avatarText () {
+      return this.email.substr(0, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
