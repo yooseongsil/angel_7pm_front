4da3a23549bdb153b9d7e169a2565bfce419c169
Donghoon Song <thdehdgns@gmail.com>
2020-07-17 21:16:49 +0900

    [add] hacks list 연동

--- src/views/hacks/HacksListPage.vue
+++ src/views/hacks/HacksListPage.vue
@@ -6,30 +6,33 @@
         <h1 class="text-h2 white--text mt-12">해커톤을 <br/>선택하세요</h1>
       </v-col>
       <v-col cols="12" :md="4"
-             v-for="(list,index) in hackLists" :key="'result-'+index">
+             v-for="(hack, index) in hackLists" :key="'result-'+index">
         <v-card>
           <v-list-item three-line>
             <v-list-item-content>
-              <div class="overline mb-4 teal--text accent-3">모인 금액: 123,456,789원</div>
-              <v-list-item-title class="headline mb-1">{{list.results.title}}</v-list-item-title>
-              <v-list-item-subtitle>{{list.results.subject}}</v-list-item-subtitle>
+              <div class="overline mb-4 teal--text accent-3">모인 금액: {{}}원</div>
+              <v-list-item-title class="headline mb-1">{{hack.title}}</v-list-item-title>
+              <v-list-item-subtitle>현재 {{}}명 신청</v-list-item-subtitle>
+              <v-list-item-content>(팀별 {{hack.team_personnel}}명)</v-list-item-content>
             </v-list-item-content>
 
             <v-list-item-avatar
               tile
               size="80"
               color="grey"
-            ></v-list-item-avatar>
+            >
+              <v-img :src="hack.img"/>
+            </v-list-item-avatar>
           </v-list-item>
 
           <v-card-actions>
-            <span>{{list.results.started_at}}</span>
+            <span>{{hack.started_at}} 19:00 시작</span>
             <v-row
               align="center"
               justify="end"
             >
             <v-btn text class="deep-purple--text accent-1"
-            @click="gotoDetail(list.results.id)">상세보기</v-btn>
+            @click="gotoDetail(hack.id)">상세보기</v-btn>
             </v-row>
           </v-card-actions>
         </v-card>
@@ -55,7 +58,7 @@ export default {
         this.count = data.count
         this.next = data.next
         this.previous = data.previous
-        this.results = data.results
+        this.hackLists = data.results
       })
         .catch(({ error }) => {
           console.log(error)
@@ -66,7 +69,7 @@ export default {
     }
   },
   created () {
-    // this.getList();
+    this.getList()
     this.hackLists = [{
       count: 0,
       next: 'test0',
