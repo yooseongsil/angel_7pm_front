7da7a04bb45f33544251829057b90b8f4bafa9e0
hanaKim <hananumber1@nate.com>
2020-07-17 13:59:35 +0900

    [add] 해커톤 생성하기 작업 초안

--- src/views/hacks/HacksCreatePage.vue
+++ src/views/hacks/HacksCreatePage.vue
@@ -6,7 +6,149 @@
         <h1 class="text-h2 white--text mt-12">해커톤을 <br/>만들기</h1>
       </v-col>
       <v-col cols="12">
-
+        <v-text-field
+          v-model="data.count"
+          label="data.count"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.next"
+          label="data.next"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.previous"
+          label="data.previous"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.id"
+          label="data.results.id"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.title"
+          label="data.results.title"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.intro"
+          label="data.results.intro"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.subject"
+          label="data.results.subject"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.rule"
+          label="data.results.rule"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.img"
+          label="data.results.img"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.fee"
+          label="data.results.fee"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.status"
+          label="data.results.status"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.started_at"
+          label="data.results.started_at"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.ended_at"
+          label="data.results.ended_at"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.judge_line"
+          label="data.results.judge_line"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.judge_day"
+          label="data.results.judge_day"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.max_personnel"
+          label="data.results.max_personnel"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.team_personnel"
+          label="data.results.team_personnel"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.awards_count"
+          label="data.results.awards_count"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.awards"
+          label="data.results.awards"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.created_at"
+          label="data.results.created_at"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.updated_at"
+          label="data.results.updated_at"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.chat_url"
+          label="data.results.chat_url"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="data.results.host"
+          label="data.results.host"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-btn block color="#BB86FC"
+               @click="createHack()"
+        >
+          만들기
+        </v-btn>
       </v-col>
     </v-row>
   </div>
@@ -46,7 +188,7 @@ export default {
     }
   }),
   methods: {
-    getList () {
+    createHack () {
       axios({
         method: 'POST',
         url: `${this.$store.state.host}/hacks/`,
