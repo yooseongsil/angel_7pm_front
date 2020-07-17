635b904fe21350e6159b857fe5868b2937006358
Donghoon Song <thdehdgns@gmail.com>
2020-07-17 21:16:41 +0900

    [add] hacks detail page

---
+++ src/views/DetailSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <section style="margin-bottom: 80px;">
+    <div class="text-h5">
+      {{title}}
+    </div>
+    <div class="text-body-2 grey--text text--lighten-1">
+      {{description}}
+    </div>
+    <slot/>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'DetailSection',
+  props: ['title', 'description']
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/hacks/TimeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-caption d-flex justify-space-between px-4 mt-5">
+    <span>{{leftText}}</span>
+    <span>{{rightText}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimeComponent',
+  props: ['leftText', 'rightText']
+}
+</script>
+
+<style scoped>
+
+</style>
