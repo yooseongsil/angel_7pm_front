7fb23aebacaa37a80f96bafb175f707c9e9d0b01
hanaKim <hananumber1@nate.com>
2020-07-17 23:03:02 +0900

    [change] tab 수정

--- src/views/hacks/TabComponent.vue
+++ src/views/hacks/TabComponent.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="p-relative">
-    <span style="z-index: 1; position: absolute; left: 24px; top: 22px;">
+  <div style="margin: -15px; position: relative">
+    <span style="z-index: 1; position: absolute; left: 12px; top: 50%; transform: translateY(-50%)">
+      <!--onclick="document.getElementsByClassName('v-slide-group__content').style.transform='translate(0)'"-->
       <v-icon>mdi-chevron-left</v-icon>
     </span>
       <v-tabs
@@ -16,16 +17,17 @@
         </v-tab>
       </v-tabs>
 
-      <v-tabs-items v-model="tab">
-        <v-tab-item
-          v-for="(item, index) in items"
-          :key="`tab-${index}`"
-        >
-          <v-card flat>
-            <v-card-text>{{ item.content }}</v-card-text>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
+    <!--items 안에 content는거 같아서, 일단 주석처리-->
+<!--      <v-tabs-items v-model="tab">-->
+<!--        <v-tab-item-->
+<!--          v-for="(item, index) in items"-->
+<!--          :key="`tab-${index}`"-->
+<!--        >-->
+<!--          <v-card flat>-->
+<!--            <v-card-text>{{ item.content }}</v-card-text>-->
+<!--          </v-card>-->
+<!--        </v-tab-item>-->
+<!--      </v-tabs-items>-->
   </div>
 </template>
 
